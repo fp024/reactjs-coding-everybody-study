09b664cbf59bb6766a7a347bc70bba6f8179ed20
fp024 <aopenasus@naver.com>
2023-02-18 21:38:04 +0900

    03. state - 진행 완료

--- Part02/03/README.md
+++ Part02/03/README.md
@@ -14,27 +14,28 @@
 
 
 
-
-
-
-
-
-
 ### 함수에서 state 사용법
 
 * 동영상 강의: https://youtu.be/R6GPIWG7O9s
 * props는 함수의 첫번째 인자를 추가해두고 그걸 사용
+* useState는 배열을 반환
+  * 인덱스 0: 현재 상태값
+  * 인덱스 1: 상태를 변경할 수 있는 set함수
 
 
 
 ## 의견
 
-* node는 `v18.14.1` 사용해보았다.
-  * create-react-app의 버전은 `5.0.1`.
-
-* 테스트 코드 조금 수정해보았는데 잘 된다.. 😄
-
-
+* 처음에 책의 코드를 안보고 진행했을 때..
+  
+  ```react
+        <FuncComp initNumber={2} date={new Date()}></FuncComp>
+        <ClassComp initNumber={2} date={new Date()}></ClassComp>
+  ```
+  
+  객체로 전달해서 에러가 났던 것 같음..   `new Date().toString()` 으로 문자열로 전달해줘야했음.
+  
+  
 
 ---
 
--- Part02/react-func-class-style-coding/src/App.js
+++ Part02/react-func-class-style-coding/src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
       <h1>Hello World</h1>
-      <FuncComp initNumber={2}></FuncComp>
-      <ClassComp initNumber={2}></ClassComp>
+      <FuncComp initNumber={2} date={new Date().toString()}></FuncComp>
+      <ClassComp initNumber={2} date={new Date().toString()}></ClassComp>
     </div>
   );
 }
 
 function FuncComp(props) {
+  const [number, setNumber] = useState(props.initNumber);
+  const [date, setDate] = useState(props.date);
+
   return (
     <div className="container">
       <h2>function style component</h2>
-      <p>Number : {props.initNumber}</p>
+      <p>Number : {number}</p>
+      <p>Date : {date}</p>
+      <input
+        type="button"
+        value="random"
+        onClick={() => {
+          setNumber(Math.random());
+        }}
+      />
+      <input
+        type="button"
+        value="date"
+        onClick={() => {
+          setDate(new Date().toString());
+        }}
+      />
     </div>
   );
 }
@@ -23,6 +41,7 @@ function FuncComp(props) {
 class ClassComp extends Component {
   state = {
     number: this.props.initNumber,
+    date: this.props.date,
   };
 
   render() {
@@ -30,13 +49,21 @@ class ClassComp extends Component {
       <div className="container">
         <h2>class style component</h2>
         <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
         <input
           type="button"
           value="random"
           onClick={() => {
             this.setState({ number: Math.random() });
           }} // 람다식으로 쓰면 bind(this)를 할 필요가 없음.
-        ></input>
+        />
+        <input
+          type="button"
+          value="date"
+          onClick={() => {
+            this.setState({ date: new Date().toString() });
+          }}
+        />
       </div>
     );
   }
