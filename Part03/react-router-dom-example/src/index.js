01b85d0068564b205502eb3020a2a274169a4bfd
fp024 <aopenasus@naver.com>
2023-02-19 23:00:07 +0900

    App 컴포넌트에 BrowserRouter 설징

--- Part03/react-router-dom-example/src/index.js
+++ Part03/react-router-dom-example/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function Home() {
   return (
@@ -42,7 +43,11 @@ function App() {
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
