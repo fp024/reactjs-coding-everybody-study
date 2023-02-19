6a66a2d0740dce7d001c9af1e0edbdbb464f90bd
fp024 <aopenasus@naver.com>
2023-02-19 22:53:04 +0900

    기본 프로젝트 준비

---
+++ Part03/02/README.md
@@ -0,0 +1,38 @@
+#### 03. [React Router DOM](../README.md) >>>
+
+---
+
+## 02. 실습 준비
+
+* 프로젝트 디렉토리: 
+
+* 동영상 강의: https://www.youtube.com/watch?v=WLdbsl9UwDc?t=138
+
+  * 동영상이 하나로 되어있음.
+
+   
+  
+
+### 실습 준비
+
+```bash
+> npx create-react-app react-router-dom-example
+```
+
+
+
+
+
+
+
+## 의견
+
+* 
+
+
+
+---
+
+## 정오표
+
+* 없음
---
+++ Part03/react-router-dom-example/src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      Home...
+    </div>
+  );
+}
+
+function Topics() {
+  return (
+    <div>
+      <h2>Topics</h2>
+      Topics...
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React Router DOM</h1>
+      <Home></Home>
+      <Topics></Topics>
+      <Contact></Contact>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
