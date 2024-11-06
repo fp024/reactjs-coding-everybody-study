170b0c332ee7f64ab626c20ae96e256cefc20d30
fp024 <aopenasus@naver.com>
2024-11-07 07:21:43 +0900

    잠시 멈춤 방식 변경

--- clean-node-modules-dir.bat
+++ clean-node-modules-dir.bat
@@ -8,7 +8,8 @@ REM 어떤 경로 이하의 node_modules 디렉토리가 전부 삭제될 것인
 echo Deleting all node_modules directories under: %ROOT_DIR%
 
 REM 루트 디렉토리 확인
-pause
+echo Press Enter key to continue...
+set /p dummyVar=""
 
 REM 모든 하위 프로젝트의 node_modules 폴더를 삭제합니다
 for /r "%ROOT_DIR%" %%d in (node_modules) do (
@@ -18,6 +19,7 @@ for /r "%ROOT_DIR%" %%d in (node_modules) do (
     )
 )
 
-pause
+echo Press Enter key to exit...
+set /p dummyVar=""
 
 endlocal
