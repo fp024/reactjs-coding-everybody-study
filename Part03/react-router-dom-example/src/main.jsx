c7c4f88634475696dc6aef2e40479649f4eaa0ff
fp024 <aopenasus@naver.com>
2025-06-18 15:24:19 +0900

    파트3. Vite 프로젝트 전환 (#1)
    "파트3. React Router DOM 편" Vite 프로젝트 전환

--- Part03/react-router-dom-example/src/main.jsx
+++ Part03/react-router-dom-example/src/main.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes, NavLink, useParams } from 'react-router-dom';
 
 const contents = [
@@ -103,14 +101,8 @@ function App() {
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
